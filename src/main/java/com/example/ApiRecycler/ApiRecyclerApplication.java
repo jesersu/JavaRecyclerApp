d7f215b734eac9d89079d0c5b7bccd97cffd6419
‘jesus’ <jesersu@gmail.com>
2024-12-25 13:09:29 -0500

    first commit

---
+++ src/main/java/com/example/ApiRecycler/ApiRecyclerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiRecycler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRecyclerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRecyclerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApiRecycler/ApiRecyclerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiRecycler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRecyclerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
